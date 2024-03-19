353467f69115054cc4609837a6049f27a619954d
Samuel Joel T <samueljoel464@gmail.com>
2024-03-19 22:22:20 +0700

    feat: adding routing and landing page index

---
+++ src/pages/landing-page/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Index = () => {
+  return (
+    <div>
+      <div>
+        <h1>SOCIAL MEDIA APP</h1>
+        <h2>Connect Freely, Share Boldly</h2>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Index;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,21 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import Home from '@/pages/Home';
+import Index from '@/pages/landing-page/index';
 
-export default function Router() {
-    const routes = [
-      {
-        path: '/home',
-        element: <Home />,
-      },
-    ];
-      const router = createBrowserRouter(routes);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/landing-page",
+    element: <Index />,
+  },
+]);
 
+const app = () => {
   return <RouterProvider router={router} />;
-}
+};
 
+export default app;
