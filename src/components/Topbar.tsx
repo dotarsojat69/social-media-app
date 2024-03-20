6efba8fc3ceb82fcd9c7270252f212a5e6c3637e
dotarsojat69 <tdeath457@gmail.com>
2024-03-21 01:45:34 +0700

    feat: merging

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,17 +1,15 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Topbar = () => {
   return (
-    <section className='topbar'>
-        <div className='flex-between py-4 px-5'>
-            <Link to= '/' className="flex gap-3 items-center">
-                Social Media App
-            </Link>
-
-        </div>
-
+    <section className="topbar">
+      <div className="flex-between py-4 px-5">
+        <Link to="/" className="flex gap-3 items-center">
+          Social Media App
+        </Link>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Topbar
+export default Topbar;
