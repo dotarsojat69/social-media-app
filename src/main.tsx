d4894556ddc6d5d27b6718386a46a6bc39bb8080
Samuel Joel T <samueljoel464@gmail.com>
2024-03-21 14:24:33 +0700

    fix: minor changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import ReactDOM from "react-dom/client";
 
 import App from "@/routes";
-import "@/global.css";
+import "@/styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <>
