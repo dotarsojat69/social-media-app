19144e82f11ebea98a7f87183c4ffa28f2b7e060
Samuel J T <155447567+LemKimia@users.noreply.github.com>
2024-03-22 19:29:02 +0700

    fix: changes

--- src/utils/apis/posts/index.ts
+++ src/utils/apis/posts/index.ts
@@ -5,7 +5,7 @@ import {
     editPosts,
     deletePosts,
   } from "./api";
-  import { DetailPost, Post, PostPayloadSchema } from "./types";
+  import { DetailPost,  PostPayloadSchema } from "./types";
   
   export { getPosts, getDetailPosts, addPosts, editPosts, deletePosts };
-  export type { Post, DetailPost, PostPayloadSchema };
+  export type {  DetailPost, PostPayloadSchema };
